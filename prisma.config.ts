4ea97c1f3e15d73dd025c4ad86712d487eacf0d4
Ibrahim Ashraf <xxibrahimashrafxx@gmail.com>
2026-07-24 17:33:33 +0300

    feat: add error handling for missing database URLs in Prisma configuration

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,12 @@ import { defineConfig } from "prisma/config";
 
 const prismaUrl = process.env.DIRECT_URL || process.env.DATABASE_URL;
 
+if (!prismaUrl) {
+  throw new Error(
+    "DIRECT_URL or DATABASEP_URL must be set in .env for Prisma CLI commands",
+  );
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
